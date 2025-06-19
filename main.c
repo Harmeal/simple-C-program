e451a132e69fe704eae93577e9948e4fe85f27e1
yassin <dahy7815@gmail.com>
2025-06-19 08:14:19 +0300

    This is programming sum 2 numbers

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fun.h>
+int main()
+{
+    int choice;
+    char name[20];
+    printf("Enter your name:\n");
+    scanf("%s", "name");
+    printf("Hello,Mr %s !\n", "name");
+    add();
+    printf("thanks for using our programing sum");
+}
